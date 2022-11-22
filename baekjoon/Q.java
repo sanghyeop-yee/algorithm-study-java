3a54297533e656f8d8e31cd37311588f362f238b
Sanghyeop Lee <61818855+sanghyeop-yee@users.noreply.github.com>
2022-11-22 14:48:32 +0900

    solve a plus b problem using for loop

--- baekjoon/Q.java
+++ baekjoon/Q.java
@@ -2,32 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Q {
-	public static void main(String args[]) throws IOException {
-		// 입력받기
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// 문자의 공백문자를 구분자로 받아서 입력받기
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		// 변수 선언
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		int prize = 0;
-		
-		// 계산
-		// 같은 눈 3개
-		if(a == b && a == c && b == c) {
-			prize = 10000 + a * 1000;
-		} else if(a == b) {
-			prize = 1000 + a * 100;
-		} else if(a == c) {
-			prize = 1000 + a * 100;
-		} else if(b == c) {
-			prize = 1000 + b * 100;
-		} else {
-			prize = Math.max(a, Math.max(b, c));
+	public static void main(String[] args) throws IOException {
+		int[] arr = {1,2,3,4,5};    
+		for(int i = 0; i < arr.length; i++) {
+		       System.out.println(arr[i]);
 		}
-		
-		System.out.println(prize);
-		
+		for(int number : arr) {
+		       System.out.println(number);
+		}
 	}
 }
