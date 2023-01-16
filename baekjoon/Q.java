bd3376ce097fb892756ffecdc143319651fecb93
Sanghyeop Lee <61818855+sanghyeop-yee@users.noreply.github.com>
2023-01-16 15:55:18 +0900

    update function solution

--- baekjoon/Q.java
+++ baekjoon/Q.java
@@ -1,9 +1,7 @@
 
 public class Q {
-	public static void main(String[] args){
-		
-		// 셀프넘버 출력하기
-		// 1-10000 인덱스에서 생성자인 경우에 표시하기
+	
+	public static void main(String args[]) {
 		boolean[] check = new boolean[10001];
 		
 		for(int i=1; i<10001; i++) {
@@ -12,19 +10,16 @@ public class Q {
 			if(n < 10001) {
 				check[n] = true;
 			}
-			
 		}
-
-		// 1-10000 까지 훑으면서 해당이 안되는 셀프넘버 출력하기 
+		
 		StringBuilder sb = new StringBuilder();
 		
 		for(int i=1; i<10001; i++) {
-			if(!check[i]) {
+			if(!check[i]) { // if check is false
 				sb.append(i).append('\n');
-			}
+			}			
 		}
 		System.out.println(sb);
-		
 	}
 	
 	public static int d(int number) {
@@ -32,10 +27,9 @@ public class Q {
 		
 		while(number != 0) {
 			sum = sum + (number % 10);
-			number = number/10;
+			number = number / 10;
 		}
 		
 		return sum;
 	}
-	
 }
